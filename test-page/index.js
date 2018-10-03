f230dba7875e33ff4c177f16af293d232e155e88
jwildfire <jwildfire@gmail.com>
2018-10-03 13:50:21 -0700

    move test pages. fix #106

--- test-page/index.js
+++ test-page/index.js
@@ -1,12 +1,3 @@
-//Load local build if in local environment.
-if (window.origin !== 'https://rhoinc.github.io') {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '../safetyOutlierExplorer.js';
-    head.appendChild(script);
-}
-
 d3.csv(
     'https://rawgit.com/RhoInc/viz-library/master/data/safetyData/ADBDS.csv',
     function(error,data) {
