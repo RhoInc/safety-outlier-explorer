dbeea761d467e576f9e5bb7f72a0efae33c9f4a7
samussiah <samussiah@gmail.com>
2019-02-07 13:34:24 -0500

    fix #114

---
+++ src/callbacks/onDraw/extendYDomain.js
@@ -0,0 +1,11 @@
+export default function extendYDomain() {
+    if (
+        this.config.y.domain[0] === this.measure.domain[0] &&
+        this.config.y.domain[1] === this.measure.domain[1] &&
+        this.config.y.domain[0]  <  this.measure.domain[1]
+    )
+        this.y_dom = [
+            this.config.y.domain[0] - this.measure.range*.01,
+            this.config.y.domain[1] + this.measure.range*.01
+        ];
+}
