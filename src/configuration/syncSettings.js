e3556697d5ddf85d02495c38730c4c08331aa002
jwildfire <jwildfire@gmail.com>
2019-05-15 16:20:44 -0700

    coerces strings to array if needed. improved detail defaults. fix #131

--- src/configuration/syncSettings.js
+++ src/configuration/syncSettings.js
@@ -24,7 +24,7 @@ export default function syncSettings(settings) {
         settings.value_col
     }] [${settings.unit_col}]\n${settings.x.label} = [${settings.x.column}]`;
 
-    //Conadd custom tooltip values
+    //add custom tooltip values
     if (settings.tooltip_cols) {
         settings.tooltip_cols.forEach(function(tooltip) {
             var obj = typeof tooltip == 'string' ? { label: tooltip, value_col: tooltip } : tooltip;
@@ -71,6 +71,62 @@ export default function syncSettings(settings) {
                 '$1'
             );
         settings.unscheduled_visit_regex = new RegExp(pattern, flags);
+    }
+
+    //handle a string argument to filters
+    if (!(settings.filters instanceof Array))
+        settings.filters = typeof settings.filters === 'string' ? [settings.filters] : [];
+
+    //handle a string argument to details
+    if (!(settings.details instanceof Array))
+        settings.details = typeof settings.details === 'string' ? [settings.details] : [];
+
+    //Define default details.
+    let defaultDetails = [{ value_col: settings.id_col, label: 'Participant ID' }];
+    if (Array.isArray(settings.filters))
+        settings.filters
+            .filter(filter => filter.value_col !== settings.id_col)
+            .forEach(filter =>
+                defaultDetails.push({
+                    value_col: filter.value_col ? filter.value_col : filter,
+                    label: filter.label
+                        ? filter.label
+                        : filter.value_col
+                        ? filter.value_col
+                        : filter
+                })
+            );
+    defaultDetails.push({ value_col: settings.value_col, label: 'Result' });
+    if (settings.normal_col_low)
+        defaultDetails.push({ value_col: settings.normal_col_low, label: 'Lower Limit of Normal' });
+    if (settings.normal_col_high)
+        defaultDetails.push({
+            value_col: settings.normal_col_high,
+            label: 'Upper Limit of Normal'
+        });
+
+    //If [settings.details] is not specified:
+    if (!settings.details) settings.details = defaultDetails;
+    else {
+        //If [settings.details] is specified:
+        //Allow user to specify an array of columns or an array of objects with a column property
+        //and optionally a column label.
+        settings.details.forEach(detail => {
+            if (
+                defaultDetails
+                    .map(d => d.value_col)
+                    .indexOf(detail.value_col ? detail.value_col : detail) === -1
+            )
+                defaultDetails.push({
+                    value_col: detail.value_col ? detail.value_col : detail,
+                    label: detail.label
+                        ? detail.label
+                        : detail.value_col
+                        ? detail.value_col
+                        : detail
+                });
+        });
+        settings.details = defaultDetails;
     }
 
     return settings;
