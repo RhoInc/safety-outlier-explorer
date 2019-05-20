14f36ea65ac38c6f533e55f1a8baafa5448fe0e7
jwildfire <jwildfire@gmail.com>
2019-05-20 09:22:59 -0700

    remove details sync logic

--- src/configuration/syncSettings.js
+++ src/configuration/syncSettings.js
@@ -80,53 +80,5 @@ export default function syncSettings(settings) {
         settings.unscheduled_visit_regex = new RegExp(pattern, flags);
     }
 
-    //Define default details.
-    let defaultDetails = [{ value_col: settings.id_col, label: 'Participant ID' }];
-    if (Array.isArray(settings.filters))
-        settings.filters
-            .filter(filter => filter.value_col !== settings.id_col)
-            .forEach(filter =>
-                defaultDetails.push({
-                    value_col: filter.value_col ? filter.value_col : filter,
-                    label: filter.label
-                        ? filter.label
-                        : filter.value_col
-                        ? filter.value_col
-                        : filter
-                })
-            );
-    defaultDetails.push({ value_col: settings.value_col, label: 'Result' });
-    if (settings.normal_col_low)
-        defaultDetails.push({ value_col: settings.normal_col_low, label: 'Lower Limit of Normal' });
-    if (settings.normal_col_high)
-        defaultDetails.push({
-            value_col: settings.normal_col_high,
-            label: 'Upper Limit of Normal'
-        });
-
-    //If [settings.details] is not specified:
-    if (!settings.details) settings.details = defaultDetails;
-    else {
-        //If [settings.details] is specified:
-        //Allow user to specify an array of columns or an array of objects with a column property
-        //and optionally a column label.
-        settings.details.forEach(detail => {
-            if (
-                defaultDetails
-                    .map(d => d.value_col)
-                    .indexOf(detail.value_col ? detail.value_col : detail) === -1
-            )
-                defaultDetails.push({
-                    value_col: detail.value_col ? detail.value_col : detail,
-                    label: detail.label
-                        ? detail.label
-                        : detail.value_col
-                        ? detail.value_col
-                        : detail
-                });
-        });
-        settings.details = defaultDetails;
-    }
-
     return settings;
 }
