e95a6c1addc1bb11357ec22926a9674a579792d7
jwildfire <jwildfire@gmail.com>
2019-08-13 22:56:07 -0700

    update version and rebuild

--- safetyOutlierExplorer.js
+++ safetyOutlierExplorer.js
@@ -133,6 +133,19 @@
             return Math.log(x) * Math.LOG10E;
         };
 
+    (function() {
+        if (typeof window.CustomEvent === 'function') return false;
+
+        function CustomEvent(event, params) {
+            params = params || { bubbles: false, cancelable: false, detail: null };
+            var evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        }
+
+        window.CustomEvent = CustomEvent;
+    })();
+
     // https://github.com/wbkd/d3-extended
     d3.selection.prototype.moveToFront = function() {
         return this.each(function() {
