d7fd4013a6461371ab9ce0e21df6b8d73b5a741b
jwildfire <jwildfire@gmail.com>
2019-08-14 14:57:36 -0700

    undo configuration folder delete

---
+++ src/configuration/webchartsSettings.js
@@ -0,0 +1,43 @@
+export default function webchartsSettings() {
+    return {
+        x: {
+            column: null, // set in ./syncSettings
+            type: null, // set in ./syncSettings
+            behavior: 'raw'
+        },
+        y: {
+            column: null, // set in ./syncSettings
+            stat: 'mean',
+            type: 'linear',
+            label: 'Value',
+            behavior: 'raw'
+        },
+        marks: [
+            {
+                per: null, // set in ./syncSettings
+                type: 'line',
+                attributes: {
+                    'clip-path': null // set in ./syncSettings
+                },
+                tooltip: null, // set in ./syncSettings
+                default: true
+            },
+            {
+                per: null, // set in ./syncSettings
+                type: 'circle',
+                attributes: {
+                    'clip-path': null // set in ./syncSettings
+                },
+                tooltip: null, // set in ./syncSettings
+                default: true
+            }
+        ],
+        resizable: true,
+        margin: {
+            right: 30, // create space for box plot
+            left: 60
+        },
+        gridlines: 'y',
+        aspect: 3
+    };
+}
